b7c82a8c0605185077377098a3561378dd00756e
Guilherme <guilhermeperrotta@live.com>
2019-08-27 19:51:58 -0300

    Fix bomb tempo

--- game/sound/sound_variables.asm
+++ game/sound/sound_variables.asm
@@ -1,5 +1,4 @@
 
-
-sound_flag_tick        .dsb 1
-sound_flag_expl        .dsb 1
-sound_frame_counter    .dsb 1
+sound_frame_counter    .dsb 1, #0
+sound_flag_tick        .dsb 1, #0
+sound_flag_expl        .dsb 1, #0
