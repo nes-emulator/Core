47d8cf26cd74d0f70d7c46737d91a67f57eded62
Guilherme <guilhermeperrotta@live.com>
2019-08-29 01:52:12 -0300

    Change step sound note

--- game/sound/engine.asm
+++ game/sound/engine.asm
@@ -25,20 +25,16 @@
 ; Sets the bomb tick flag
 sound_bomb_tick:
     PHA
-
     LDA #1
     STA sound_flag_tick
-
     PLA
     RTS
 
 ; Sets the bomb explosion flag
 sound_bomb_expl:
     PHA
-
     LDA #1
     STA sound_flag_expl
-
     PLA
     RTS
 
@@ -64,7 +60,9 @@ sound_init:
 
     ; Clear counters and flags
     LDA #0
-    STA sound_frame_counter
+    STA sound_tick_counter
+    STA sound_expl_counter
+    STA sound_step_counter
     STA sound_flag_tick
     STA sound_flag_expl
 
@@ -74,6 +72,16 @@ Beep:
     LDA #%10111111 ; Duty 10 (50%), volume 15
     STA SQ1_ENV
     LDA #$C9    ;0C9 is a C# in NTSC mode
+    STA SQ1_LO
+    LDA #$00
+    STA SQ1_HI
+    RTS
+
+; $0042
+Step:
+    LDA #%10110100 ; Duty 10 (50%), volume 4
+    STA SQ1_ENV
+    LDA #$42
     STA SQ1_LO
     LDA #$00
     STA SQ1_HI
@@ -125,15 +133,15 @@ sound_play_frame:
         LDA sound_flag_step
         CMP #1
         BMI explosion
-            JSR Beep
-            INC sound_frame_counter
-            LDA sound_frame_counter
+            JSR Step
+            INC sound_step_counter
+            LDA sound_step_counter
             CMP #STEP_TEMPO
             BMI sound_done
                 ;end SFX
                 LDA #0
                 STA sound_flag_step
-                STA sound_frame_counter
+                STA sound_step_counter
                 JSR ClearSQ1
 
     explosion:
@@ -141,14 +149,14 @@ sound_play_frame:
         CMP #1
         BMI tick
             JSR Beep
-            INC sound_frame_counter
-            LDA sound_frame_counter
+            INC sound_expl_counter
+            LDA sound_expl_counter
             CMP #EXPL_TEMPO
             BMI sound_done
                 ; end SFX
                 LDA #0
                 STA sound_flag_expl
-                STA sound_frame_counter
+                STA sound_expl_counter
                 JSR ClearSQ1
 
     tick:
@@ -157,14 +165,14 @@ sound_play_frame:
         BMI sound_done
             ; play the tick
             JSR TriangleOn
-            INC sound_frame_counter
-            LDA sound_frame_counter
+            INC sound_tick_counter
+            LDA sound_tick_counter
             CMP #TICK_TEMPO
             BMI sound_done
                 ; end SFX
                 LDA #0
                 STA sound_flag_tick
-                STA sound_frame_counter
+                STA sound_tick_counter
                 JSR TriangleOff
 
     sound_done:
--- game/sound/sound_variables.asm
+++ game/sound/sound_variables.asm
@@ -1,5 +1,7 @@
 
-sound_frame_counter    .dsb 1, #0
+sound_tick_counter    .dsb 1, #0
+sound_expl_counter    .dsb 1, #0
+sound_step_counter    .dsb 1, #0
 sound_flag_tick        .dsb 1, #0
 sound_flag_expl        .dsb 1, #0
 sound_flag_step        .dsb 1, #0
