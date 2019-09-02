66f548a8a535504820007af5fd7ace11876a0b9c
edinha <edinha.gon.cruz@gmail.com>
2019-09-02 09:44:13 -0300

    start of base classes

---
+++ emulator/instruction/instruction.py
@@ -0,0 +1,15 @@
+
+
+class Instruction:
+    opcode = ''
+    cycles = 1
+
+    def __init__(self, opcode, cycles):
+        self.opcode = opcode
+        self.cycles = cycles
+
+    def execute(self, params):
+        pass
+
+    def get_cycles(self):
+        return self.cycles
---
+++ emulator/register/register.py
@@ -0,0 +1,19 @@
+
+
+class Register:
+    value = int('00000000', 2)
+
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def shift_left(self, carry):
+        pass
+
+    def shift_right(self, carry):
+        pass
