3849e64c5f5415502ac6554b4f3c345361b96f04
edinha <edinha.gon.cruz@gmail.com>
2019-09-06 21:17:28 -0300

    Renaming mapper

--- src/instruction/collection.py
+++ src/instruction/collection.py
@@ -4,9 +4,9 @@ from .implementation import *
 def _mount_all_instructions_list():
     return [ i() for i in Instruction.__subclasses__() ]
 
-class InstructionMapper:
+class InstructionCollection:
     all_instructions = { i.opcode: i for i in _mount_all_instructions_list() }
 
     @staticmethod
     def get_instruction(opcode):
-        return InstructionMapper.all_instructions.get(opcode, Instruction('none', 1))
+        return InstructionCollection.all_instructions.get(opcode, Instruction('none', 1))
--- src/instruction/test.py
+++ src/instruction/test.py
@@ -1,15 +1,15 @@
 import unittest
-from .mapper import InstructionMapper
+from .collection import InstructionCollection
 from .instruction import Instruction
 
 class RegisterTest(unittest.TestCase):
 
     def test_get_instruction_add(self):
-        inst = InstructionMapper.get_instruction('add')
+        inst = InstructionCollection.get_instruction('add')
         self.assertEqual(1, inst.get_cycles())
         self.assertEqual('add', inst.opcode)
 
     def test_get_instruction_none(self):
-        inst = InstructionMapper.get_instruction('inst-non-existent-bla-bla')
+        inst = InstructionCollection.get_instruction('inst-non-existent-bla-bla')
         self.assertEqual(1, inst.get_cycles())
         self.assertEqual('none', inst.opcode)
