84a865b2dc5919ed32d89dd970977a9056402eb5
Guilherme <guilhermeperrotta@live.com>
2019-09-09 20:28:15 -0300

    Add State Class

---
+++ src/state.py
@@ -0,0 +1,11 @@
+import src.register.register as reg
+import src.register.statusregister as st_reg
+
+class State:
+
+    def __init__(self):
+        self.a = reg.Register()
+        self.x = reg.Register()
+        self.y = reg.Register()
+        self.status = st_reg.StatusRegister()
+
