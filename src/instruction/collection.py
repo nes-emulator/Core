29fef02c2b2896227f25c79328dab385d276aa0b
vitor andrietta <vitorandrietta@gmail.com>
2019-09-13 06:45:55 -0300

    collection extraction now extract instructions and instantiate them based in addressing mode - actually returning empty in .__subclasses__()

--- src/instruction/collection.py
+++ src/instruction/collection.py
@@ -2,12 +2,16 @@ from .instruction import Instruction
 from .implementation import *
 
 
-def _mount_all_instructions_list():
-    return [i() for i in Instruction.__subclasses__()]
+def _mount_all_instructions():
+    instructions = {}
+    for instruction in Instruction.__subclasses__():
+        for opcode, addr_mode in instruction.included_addr_modes.items():
+            instructions[opcode] = instructions(opcode, addr_mode)
+    return instructions
 
 
 class InstructionCollection:
-    all_instructions = {i.opcode: i for i in _mount_all_instructions_list()}
+    all_instructions = _mount_all_instructions()
 
     @staticmethod
     def get_instruction(opcode):
