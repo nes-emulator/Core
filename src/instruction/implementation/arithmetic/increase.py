260070644385cee5ae30aca47350799c5c2cf397
edinha <edinha.gon.cruz@gmail.com>
2019-09-17 15:01:50 -0300

    Increase and decrease basic instructions

---
+++ src/instruction/implementation/arithmetic/increase.py
@@ -0,0 +1,37 @@
+from src.instruction.instruction import *
+from src.instruction.addressing import *
+
+class INX(Instruction):
+    def __init__(self):
+        super().__init__(opcode=232, cycles=1)
+
+    def execute(self, memory, cpu, params):
+        new_calculated = cpu.state.x.get_value() + 1
+
+        new_reg_value = new_calculated % 256
+        cpu.state.x.set_value(new_reg_value)
+
+        cpu.state.status.zero = (new_reg_value == 0)
+        cpu.state.status.negative = (new_reg_value > 127)
+
+class INY(Instruction):
+    def __init__(self):
+        super().__init__(opcode=200, cycles=1)
+
+    def execute(self, memory, cpu, params):
+        new_calculated = cpu.state.y.get_value() + 1
+
+        new_reg_value = new_calculated % 256
+        cpu.state.y.set_value(new_reg_value)
+
+        cpu.state.status.zero = (new_reg_value == 0)
+        cpu.state.status.negative = (new_reg_value > 127)
+
+# TODO Review thsi 136
+class INC(Instruction):
+    def __init__(self):
+        super().__init__(opcode='inc', cycles=1)
+
+    def execute(self, memory, cpu, params):
+
+        pass
