d9c92f8fc62e1f8fb53387d4bbb5afd1b333a2eb
vitor andrietta <vitorandrietta@gmail.com>
2019-09-18 11:49:09 -0300

    fixed - log formatation

--- src/cpu/logger.py
+++ src/cpu/logger.py
@@ -8,11 +8,11 @@ class Logger:
     # if file is not provided the log will be printed in the standard output
     @classmethod
     def log_reg_status(cls, state, out_file=None):
-        hex_regs = [" pc = {} ".format(hex(state.pc)), "a = {} ".format(state.a),
-                    "x = {} ".format(state.x), "y = {} ".format(state.y),
-                    "sp = {} ".format(state.sp)]
+        hex_regs = [" pc = {} ".format(hex(state.pc.get_value())), " a = {} ".format(hex(state.a.get_value())),
+                    " x = {} ".format(hex(state.x.get_value())), " y = {} ".format(hex(state.y.get_value())),
+                    " sp = {} ".format(hex(state.sp.get_value()))]
 
-        output = Logger.SEPARATOR + Logger.SEPARATOR.join(hex_regs) + Logger.SEPARATOR
+        output = Logger.SEPARATOR + Logger.SEPARATOR.join(hex_regs)
 
         flag_reg_out = Logger.SEPARATOR + " p[NV-BDIZC] = {} " + Logger.SEPARATOR
         flags_int = [int(state.status.negative), int(state.status.overflow),
@@ -41,7 +41,11 @@ class Logger:
 
     # log change mem vals
     @classmethod
-    def log_mem_manipulation(cls, memory, addr, out_file):
+    def log_mem_manipulation(cls, mem, addr, out_file=None):
         out_mem = " MEM[{}] = {} " + Logger.SEPARATOR
-        out_mem = out_mem.format(hex(addr), hex(memory.retrieve_content(addr)))
-        pass
+        out_mem = out_mem.format(hex(addr), hex(mem.retrieve_content(addr)))
+        if out_file is not None:
+            with open(out_file, "w+") as f:
+                f.write(out_mem)
+        else:
+            print(out_mem, end="")
