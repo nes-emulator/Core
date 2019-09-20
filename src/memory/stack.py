f4a56a0155e52376c651d0c3a16249e47fe66635
vitor andrietta <vitorandrietta@gmail.com>
2019-09-20 20:20:45 -0300

    stack improved - now pc is stored in 2 different memory pos

--- src/memory/stack.py
+++ src/memory/stack.py
@@ -1,5 +1,6 @@
 from src.cpu.cpu import state
 from src.register import statusregister
+from src.util.util import apply_higher_byte_mask, apply_lower_byte_mask, make_16b_binary
 
 
 class Stack:
@@ -7,16 +8,21 @@ class Stack:
     START_ADDR = 0x1FF
     END_ADDR = 0x100
 
+    def pop_pc(self):
+        low_byte = self.pop_val()
+        high_byte = self.pop_val()
+        self.state.pc.set_value(make_16b_binary(high_byte, low_byte))
+
+    def push_pc(self):
+        pc_low_byte = apply_lower_byte_mask(self.state.pc.get_value())
+        pc_high_byte = apply_lower_byte_mask(self.state.pc.get_value())
+        self.push_val(pc_high_byte)
+        self.push_val(pc_low_byte)
+
     def __init__(self, memory, cpu_state):
         self.memory = memory
         self.state = cpu_state
         self.set_top(Stack.START_ADDR)
-
-    def push_val(self, val):
-        if self.full_stk():
-            raise ("stack overflow")
-        self.memory.set_content(self.get_top(), val)
-        self.set_top(self.get_top() - 1)  # points to a empty pos
 
     def pop_val(self):
         if self.empty_stk():
@@ -24,27 +30,35 @@ class Stack:
         self.set_top(self.get_top() + 1)
         return self.memory.retrieve_content(self.get_top())
 
-    # x
-    # y
-    # pc
-    # status
-    # save state
     def push_regs(self):
         self.push_val(self.state.x.get_value())
         self.push_val(self.state.y.get_value())
-        self.push_val(self.state.pc.get_value())
+        self.push_pc(self.state.pc.get_value())
         self.push_val(self.state.status.to_val())
 
     # x
     # y
     # pc
     # status
-    # retrieve state
+    # save state
     def pop_regs(self):
         self.state.status = statusregister.StatusRegister(self.pop_val())
-        self.state.pc.set_value(self.pop_val())
+        self.pop_pc()
         self.state.y.set_value(self.pop_val())
         self.state.x.set_value(self.pop_val())
+
+    # x
+    # y
+    # pc
+    # status
+    def push_val(self, val):
+
+        if self.full_stk():
+            raise ("stack overflow")
+        self.memory.set_content(self.get_top(), val)
+        self.set_top(self.get_top() - 1)  # points to a empty pos
+
+    # retrieve state
 
     def empty_stk(self):
         return self.top_addr == Stack.START_ADDR
