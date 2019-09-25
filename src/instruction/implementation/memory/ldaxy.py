58fc46415ea4378493c2d27329e54ea380c5e3f9
Guilherme <guilhermeperrotta@live.com>
2019-09-25 00:01:24 -0300

    Add LDX and LDY

---
+++ src/instruction/implementation/memory/ldaxy.py
@@ -0,0 +1,64 @@
+from src.instruction.instruction import *
+from src.instruction.addressing.addressing import *
+
+class LoadX(CalculateAddress, Executable):
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        # A <- M
+        val = memory.retrieve_content(addr)
+        cpu.state.x.set_value(val)
+
+        # update status register
+        cpu.state.status.zero = (val == 0)
+        cpu.state.status.negative = (val > 127)
+
+class LdxImmediate(Instruction, ImmediateAddr, LoadX):
+    def __init__(self):
+        super().__init__(opcode = 0xA2, cycles = 2)
+
+class LdxImmediate(Instruction, ZeroPageAddr, LoadX):
+    def __init__(self):
+        super().__init__(opcode = 0xA6, cycles = 3)
+
+class LdxImmediate(Instruction, ZeroPgDirectIndexedRegYAddr, LoadX):
+    def __init__(self):
+        super().__init__(opcode = 0xB6, cycles = 4)
+
+class LdxImmediate(Instruction, AbsoluteAddr, LoadX):
+    def __init__(self):
+        super().__init__(opcode = 0xAE, cycles = 4)
+
+class LdxImmediate(Instruction, AbsDirectIndexedRegYAddr, LoadX):
+    def __init__(self):
+        super().__init__(opcode = 0xBE, cycles = 4)
+
+class LoadY(CalculateAddress, Executable):
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        # A <- M
+        val = memory.retrieve_content(addr)
+        cpu.state.y.set_value(val)
+
+        # update status register
+        cpu.state.status.zero = (val == 0)
+        cpu.state.status.negative = (val > 127)
+
+class LdyImmediate(Instruction, ImmediateAddr, LoadY):
+    def __init__(self):
+        super().__init__(opcode = 0xA0, cycles = 2)
+
+class LdyZeroPage(Instruction, ZeroPageAddr, LoadY):
+    def __init__(self):
+        super().__init__(opcode = 0xA4, cycles = 3)
+
+class LdyZeroPageX(Instruction, ZeroPgDirectIndexedRegXAddr, LoadY):
+    def __init__(self):
+        super().__init__(opcode = 0xB4, cycles = 4)
+
+class LdyAbsolute(Instruction, AbsoluteAddr, LoadY):
+    def __init__(self):
+        super().__init__(opcode = 0xAC, cycles = 4)
+
+class LdyAbsoluteX(Instruction, AbsDirectIndexedRegXAddr, LoadY):
+    def __init__(self):
+        super().__init__(opcode = 0xBC, cycles = 4)
--- src/instruction/tests/memory/load.py
+++ src/instruction/tests/memory/load.py
@@ -12,6 +12,8 @@ class LoadTest(unittest.TestCase):
         self.memory.set_content(101, 0)
         self.memory.set_content(102, 128)
         self.cpu.state.a.set_value(1)
+        self.cpu.state.x.set_value(1)
+        self.cpu.state.y.set_value(1)
 
     def lda_change_a(self):
         opcode = 0xA9
@@ -22,6 +24,48 @@ class LoadTest(unittest.TestCase):
 
     def lda_change_status(self):
         opcode = 0xA5
+        inst = InstructionCollection.get_instruction(opcode)
+
+        # zero
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[101])
+        self.assertTrue(self.cpu.state.status.zero)
+        self.assertFalse(self.cpu.state.status.negative)
+
+        # negative
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[102])
+        self.assertFalse(self.cpu.state.status.zero)
+        self.assertTrue(self.cpu.state.status.negative)
+
+    def ldx_change_x(self):
+        opcode = 0xA2
+        inst = InstructionCollection.get_instruction(opcode)
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[100])
+
+        self.assertEqual(self.cpu.state.x.get_value, 77)
+
+    def ldx_change_status(self):
+        opcode = 0xA6
+        inst = InstructionCollection.get_instruction(opcode)
+
+        # zero
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[101])
+        self.assertTrue(self.cpu.state.status.zero)
+        self.assertFalse(self.cpu.state.status.negative)
+
+        # negative
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[102])
+        self.assertFalse(self.cpu.state.status.zero)
+        self.assertTrue(self.cpu.state.status.negative)
+
+    def ldy_change_y(self):
+        opcode = 0xA0
+        inst = InstructionCollection.get_instruction(opcode)
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[100])
+
+        self.assertEqual(self.cpu.state.y.get_value, 77)
+
+    def ldy_change_status(self):
+        opcode = 0xA4
         inst = InstructionCollection.get_instruction(opcode)
 
         # zero
