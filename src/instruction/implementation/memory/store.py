484ede4aada16b3ebad5210b5da76f1c6892fcb1
Guilherme <guilhermeperrotta@live.com>
2019-09-25 00:23:43 -0300

    Add STA, STX, STY

---
+++ src/instruction/implementation/memory/store.py
@@ -0,0 +1,78 @@
+from src.instruction.instruction import *
+from src.instruction.addressing.addressing import *
+
+class StoreA(CalculateAddress, Executable):
+
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        memory.set_content(addr, cpu.state.a.get_value())
+
+class StoreX(CalculateAddress, Executable):
+
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        memory.set_content(addr, cpu.state.x.get_value())
+
+class StoreY(CalculateAddress, Executable):
+
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        memory.set_content(addr, cpu.state.y.get_value())
+
+##### STA
+
+class StaZeroPage(Instruction, ZeroPageAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x85, cycles = 3)
+
+class StaZeroPageX(Instruction, ZeroPgDirectIndexedRegXAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x95, cycles = 4)
+
+class StaAbsolute(Instruction, AbsoluteAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x80, cycles = 4)
+
+class StaAbsoluteX(Instruction, AbsDirectIndexedRegXAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x90, cycles = 5)
+
+class StaAbsoluteY(Instruction, AbsDirectIndexedRegYAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x99, cycles = 5)
+
+class StaIndirectX(Instruction, IndirectPreIndexedAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x81, cycles = 6)
+
+class StaIndirectY(Instruction, IndirectPostIndexedAddr, StoreA):
+    def __init__(self):
+        super().__init__(opcode = 0x91, cycles = 6)
+
+##### STX
+
+class StxZeroPage(Instruction, ZeroPageAddr, StoreX):
+    def __init__(self):
+        super().__init__(opcode = 0x86, cycles = 3)
+
+class StxZeroPageY(Instruction, ZeroPgDirectIndexedRegYAddr, StoreX):
+    def __init__(self):
+        super().__init__(opcode = 0x96, cycles = 4)
+
+class StxAbsolute(Instruction, AbsoluteAddr, StoreX):
+    def __init__(self):
+        super().__init__(opcode = 0x8E, cycles = 4)
+
+##### STY
+
+class StyZeroPage(Instruction, ZeroPageAddr, StoreY):
+    def __init__(self):
+        super().__init__(opcode = 0x84, cycles = 3)
+
+class StyZeroPageX(Instruction, ZeroPgDirectIndexedRegXAddr, StoreY):
+    def __init__(self):
+        super().__init__(opcode = 0x94, cycles = 4)
+
+class StyAbsolute(Instruction, AbsoluteAddr, StoreY):
+    def __init__(self):
+        super().__init__(opcode = 0x8C, cycles = 4)
---
+++ src/instruction/tests/memory/store.py
@@ -0,0 +1,35 @@
+import unittest
+from src.instruction.collection import InstructionCollection
+from src.cpu.cpu import CPU
+from src.memory.memory import Memory
+
+class StoreTest(unittest.TestCase):
+    def setUp(self):
+        self.cpu = CPU()
+        self.memory = Memory()
+        # mock state
+        self.cpu.state.a.set_value(77)
+        self.cpu.state.x.set_value(88)
+        self.cpu.state.y.set_value(99)
+        self.memory.set_content(100, 55)
+
+    def sta_change_mem(self):
+        opcode = 0x85
+        inst = InstructionCollection.get_instruction(opcode)
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[100])
+
+        self.assertEqual(self.memory.retrieve_content(100), 77)
+
+    def stx_change_mem(self):
+        opcode = 0x86
+        inst = InstructionCollection.get_instruction(opcode)
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[100])
+
+        self.assertEqual(self.memory.retrieve_content(100), 88)
+
+    def sty_change_mem(self):
+        opcode = 0x84
+        inst = InstructionCollection.get_instruction(opcode)
+        inst.execute(memory=self.memory, cpu=self.cpu, params=[100])
+
+        self.assertEqual(self.memory.retrieve_content(100), 99)
