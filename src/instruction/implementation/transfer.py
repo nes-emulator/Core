86d10e32c35db6ab7301dd0da922e84dc976b71b
Guilherme <guilhermeperrotta@live.com>
2019-09-25 03:41:47 -0300

    Fix transfer register access

--- src/instruction/implementation/transfer.py
+++ src/instruction/implementation/transfer.py
@@ -8,7 +8,7 @@ class Tax(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # A -> X
         a = self.cpu.state.a.get_value()
-        self.cpu.state.x = a
+        self.cpu.state.x.set_value() = a
 
         # update status register
         cpu.state.status.zero = (a == 0)
@@ -21,7 +21,7 @@ class Tay(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # A -> Y
         a = self.cpu.state.a.get_value()
-        self.cpu.state.y = a
+        self.cpu.state.y.set_value() = a
 
         # update status register
         cpu.state.status.zero = (a == 0)
@@ -34,7 +34,7 @@ class Tsx(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # SP -> X
         sp = self.cpu.state.sp.get_value()
-        self.cpu.state.x = sp
+        self.cpu.state.x.set_value() = sp
 
         # update status register
         cpu.state.status.zero = (sp == 0)
@@ -47,7 +47,7 @@ class Txa(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # X -> A
         x = self.cpu.state.x.get_value()
-        self.cpu.state.a = x
+        self.cpu.state.a.set_value() = x
 
         # update status register
         cpu.state.status.zero = (x == 0)
@@ -60,7 +60,7 @@ class Txs(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # X -> SP
         x = self.cpu.state.x.get_value()
-        self.cpu.state.sp = x
+        self.cpu.state.sp.set_value() = x
 
 class Tya(Instruction, ImpliedAddr):
     def __init__(self):
@@ -69,7 +69,7 @@ class Tya(Instruction, ImpliedAddr):
     def execute(self, memory, cpu, params):
         # A <- Y
         y = self.cpu.state.y.get_value()
-        self.cpu.state.a = y
+        self.cpu.state.a.set_value() = y
 
         # update status register
         cpu.state.status.zero = (y == 0)
