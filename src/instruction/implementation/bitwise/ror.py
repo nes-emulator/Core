0e817a6335148912468b34534c871375b7c37913
vitor andrietta <vitorandrietta@gmail.com>
2019-09-26 18:42:08 -0300

    ROR

---
+++ src/instruction/implementation/bitwise/ror.py
@@ -0,0 +1,53 @@
+from src.instruction.instruction import *
+from src.instruction.addressing.addressing import *
+from src.util.util import apply_mask, is_negative
+from src.memory.memory import Memory
+
+
+def base_calculation(num, cpu):
+    if (cpu.state.status.carry):
+        num = num | 0x100
+    cpu.state.status.carry = bool(apply_mask(num, 0x01))
+    num = num >> 1
+    cpu.state.status.negative = is_negative(num, Memory.WORD_SIZE)
+    cpu.state.status.zero = (num == 0)
+    return num
+
+
+class RorMemory(Executable, CalculateAddress):
+    def execute(self, memory, cpu, params):
+        addr = self.calculate_unified_parameter(params, cpu, memory)
+        num = memory.retrieve_content(addr)
+        num = base_calculation(num, cpu)
+        memory.set_content(addr, num)
+        return addr
+
+
+class RorAccumulator(Instruction, AccumulatorAddr, CalculateAddress):
+    def execute(self, memory, cpu, params):
+        val_reg_a = self.calculate_unified_parameter(params, cpu, memory)
+        val_reg_a = base_calculation(val_reg_a, cpu)
+        cpu.state.a.set_value(val_reg_a)
+
+    def __init__(self):
+        super().__init__(opcode=0x6A, cycles=2)
+
+
+class RorZeroPg(Instruction, ZeroPageAddr, RorMemory):
+    def __init__(self):
+        super().__init__(opcode=0x66, cycles=5)
+
+
+class RorZeroPgXIndexed(Instruction, ZeroPgDirectIndexedRegXAddr, RorMemory):
+    def __init__(self):
+        super().__init__(opcode=0x76, cycles=6)
+
+
+class RolAbsolute(Instruction, AbsoluteAddr, RorMemory):
+    def __init__(self):
+        super().__init__(opcode=0x6E, cycles=6)
+
+
+class RorAbsoluteXIndexed(Instruction, AbsDirectIndexedRegXAddr, RorMemory):
+    def __init__(self):
+        super().__init__(opcode=0x7E, cycles=7)
--- src/instruction/tests/bitwise/test.py
+++ src/instruction/tests/bitwise/test.py
@@ -103,3 +103,32 @@ class RolTest(unittest.TestCase):
         self.assertEqual(self.cpu.state.status.negative, True)
         self.assertEqual(self.cpu.state.status.zero, False)
         self.assertEqual(self.cpu.state.status.carry, True)
+
+
+class RorTest(unittest.TestCase):
+    def setUp(self):
+        self.cpu = CPU()
+        self.memory = Memory()
+        self.memory.set_content(0xA000, 0b0000000011)
+        self.cpu.state.a.set_value(0b00000010)
+
+    def test_ror_accumulator_flags(self):
+        self.cpu.state.status.carry = True
+        inst = InstructionCollection.get_instruction(0x6A)
+        ret = inst.execute(self.memory, self.cpu, [])
+        self.assertEqual(self.cpu.state.a.get_value(), 0b10000001)
+        self.assertEqual(self.cpu.state.status.negative, True)
+        self.assertEqual(self.cpu.state.status.zero, False)
+        self.assertEqual(self.cpu.state.status.carry, False)
+        self.assertEqual(ret, None)
+
+    def test_ror_abs_flags(self):
+        params = [0, 0xA0]
+        self.cpu.state.status.carry = False
+        inst = InstructionCollection.get_instruction(0x6E)
+        ret = inst.execute(self.memory, self.cpu, params)
+        self.assertEqual(0xA000, ret)
+        self.assertEqual(self.memory.retrieve_content(0XA000), 0b00000001)
+        self.assertEqual(self.cpu.state.status.negative, False)
+        self.assertEqual(self.cpu.state.status.zero, False)
+        self.assertEqual(self.cpu.state.status.carry, True)
