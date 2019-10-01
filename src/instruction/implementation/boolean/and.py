faf61dac54c51bf144c21282cd17d0c3ca120dea
vitor andrietta <vitorandrietta@gmail.com>
2019-10-01 12:20:32 -0300

    AND  now returns the manipulated address

--- src/instruction/implementation/boolean/and.py
+++ src/instruction/implementation/boolean/and.py
@@ -2,48 +2,63 @@ from src.instruction.instruction import *
 from src.instruction.addressing.addressing import *
 
 
-class AndInstructionBase(CalculateAddress, Executable):
+def and_base_exec(cpu, value):
+    new_calculated_value = cpu.state.a.get_value() & value
+
+    new_reg_value = new_calculated_value % 256
+    cpu.state.a.set_value(new_reg_value)
+
+    cpu.state.status.zero = (new_reg_value == 0)
+    cpu.state.status.negative = (new_reg_value > 127)
+
+
+class AndInstructionMemoryBase(CalculateAddress, Executable):
     def execute(self, memory, cpu, params):
         address = self.calculate_unified_parameter(params, cpu, memory)
         value = self.retrieve_address_data(memory, address)
-
-        new_calculated_value = cpu.state.a.get_value() & value
-
-        new_reg_value = new_calculated_value % 256
-        cpu.state.a.set_value(new_reg_value)
-
-        cpu.state.status.zero = (new_reg_value == 0)
-        cpu.state.status.negative = (new_reg_value > 127)
+        and_base_exec(cpu, value)
+        return address
 
 
-class AndInstructionImmediateAddr(Instruction, ImmediateAddr, AndInstructionBase):
+class AndInstructionImmediateAddr(Instruction, ImmediateAddr):
     def __init__(self):
         super().__init__(opcode=0x29, cycles=2)
 
-class AndInstructionZeroPageAddr(Instruction, ZeroPageAddr, AndInstructionBase):
+    def execute(self, memory, cpu, params):
+        value = self.calculate_unified_parameter(params, cpu, memory)
+        and_base_exec(cpu, value)
+
+
+class AndInstructionZeroPageAddr(Instruction, ZeroPageAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x25, cycles=3)
 
-class AndInstructionZeroPgDirectIndexedRegXAddr(Instruction, ZeroPgDirectIndexedRegXAddr, AndInstructionBase):
+
+class AndInstructionZeroPgDirectIndexedRegXAddr(Instruction, ZeroPgDirectIndexedRegXAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x35, cycles=4)
 
-class AndInstructionAbsoluteAddr(Instruction, AbsoluteAddr, AndInstructionBase):
+
+class AndInstructionAbsoluteAddr(Instruction, AbsoluteAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x2D, cycles=4)
 
-class AndInstructionAbsDirectIndexedRegXAddr(Instruction, AbsDirectIndexedRegXAddr, AndInstructionBase):
+
+class AndInstructionAbsDirectIndexedRegXAddr(Instruction, AbsDirectIndexedRegXAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x3D, cycles=4)
 
-class AndInstructionAbsDirectIndexedRegYAddr(Instruction, AbsDirectIndexedRegYAddr, AndInstructionBase):
+
+class AndInstructionAbsDirectIndexedRegYAddr(Instruction, AbsDirectIndexedRegYAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x39, cycles=4)
 
-class AndInstructionIndirectPreIndexedAddr(Instruction, IndirectPreIndexedAddr, AndInstructionBase):
+
+class AndInstructionIndirectPreIndexedAddr(Instruction, IndirectPreIndexedAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x21, cycles=6)
 
-class AndInstructionIndirectPostIndexedAddr(Instruction, IndirectPostIndexedAddr, AndInstructionBase):
+
+class AndInstructionIndirectPostIndexedAddr(Instruction, IndirectPostIndexedAddr, AndInstructionMemoryBase):
     def __init__(self):
         super().__init__(opcode=0x31, cycles=5)
