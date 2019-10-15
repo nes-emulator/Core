d0bdb34922444d190c9265779b05b891578e9cf1
vitor andrietta <vitorandrietta@gmail.com>
2019-10-15 19:18:01 -0300

    added - pypy dependencies

--- setup_pypy.sh
+++ setup_pypy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 DIR="./pypyenv"
 if [ ! -d "$DIR" ]; then
-  rm -rf pypyenv pypy3.5-v7.0.0-linux64 pypy3.5-v7.0.0-linux64.tar.bz2
-  sudo apt-get install python3-pip;sudo pip3 install virtualenv;wget https://bitbucket.org/pypy/pypy/downloads/pypy3.5-v7.0.0-linux64.tar.bz2
- tar xf pypy3.5-v7.0.0-linux64.tar.bz2; rm pypy3.5-v7.0.0-linux64.tar.bz2; virtualenv -p ./pypy3.5-v7.0.0-linux64/bin/pypy3 pypyenv
+ rm -rf pypyenv pypy3.5-v7.0.0-linux64 pypy3.5-v7.0.0-linux64.tar.bz2
+ sudo apt-get install python3-pip;sudo pip3 install virtualenv;wget https://bitbucket.org/pypy/pypy/downloads/pypy3.5-v7.0.0-linux64.tar.bz2
+ tar xf pypy3.5-v7.0.0-linux64.tar.bz2; rm pypy3.5-v7.0.0-linux64.tar.bz2; virtualenv -p ./pypy3.5-v7.0.0-linux64/bin/pypy3 pypyenv;
+ source ./pypyenv/bin/activate; sudo apt-get build-dep python3-pygame; sudo apt-get install python-dev;sudo apt install libsdl1.2-dev ;
+ sudo apt-get install python-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev   libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev;
+ pypyenv/bin/pypy3 -m pip install pygame;
 fi
 
 source ./pypyenv/bin/activate
