9b299f0207cce44628a4333d063ca1a367cf7bc0
vitor andrietta <vitorandrietta@gmail.com>
2019-10-28 17:46:58 -0300

    pygame dependencies removed from tests

--- src/ppu/control/ppu_init.py
+++ src/ppu/control/ppu_init.py
@@ -1,7 +1,7 @@
 import multiprocessing as mp
 from src.ppu.driver import ppu_main
 from src.memory.ppu import PPUMemory
-from src.ppu.pygame.controller import Controllers
+#from src.ppu.pygame.controller import Controllers
 
 class PPU_Runner_Initializer:
     #   mp.set_start_method('spawn')
@@ -10,7 +10,9 @@ class PPU_Runner_Initializer:
     @classmethod
     def init_ppu(cls, ppu_mem: PPUMemory):
         ppu_process = mp.Process(target=ppu_main, args=(ppu_mem.get_regs(), ppu_mem.get_memory(),
-        ppu_mem.get_oam(),Controllers.ctrl1_btn_states, Controllers.ctrl2_btn_states),)
+        ppu_mem.get_oam(),))
+        # ppu_process = mp.Process(target=ppu_main, args=(ppu_mem.get_regs(), ppu_mem.get_memory(),
+        # ppu_mem.get_oam(),Controllers.ctrl1_btn_states, Controllers.ctrl2_btn_states),)
         ppu_process.start()
 
     @classmethod
