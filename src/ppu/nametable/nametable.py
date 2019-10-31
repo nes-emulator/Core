9dbc775c21f63af4d283bf38bb527bd89948c442
Guilherme <guilhermeperrotta@live.com>
2019-10-30 23:22:38 -0300

    Clean up Nametable Class

--- src/ppu/nametable/nametable.py
+++ src/ppu/nametable/nametable.py
@@ -1,19 +1,12 @@
 class Nametable:
 
     def __init__(self):
-        # TODO: alloc nametable 960 bytes here
-        # self.memory = array(Memory.UNSIGNED_BYTE_TYPE, (0,) * Memory.MEMORY_LIMIT)
-        self.attribute_table = array(Memory.UNSIGNED_BYTE_TYPE, (0,) * 64)
+        pass
 
     @staticmethod
     # each byte controls the palette of a 32x32 pixel area
     def get_bits(byte):
         # each byte is divided into four 2-bit areas
-        #bottomright = byte & 0b00000011
-        #bottomleft = byte & 0b00001100
-        #topright = byte & 0b00110000
-        #topleft = byte & 0b11000000
-
         topleft = byte & 0b00000011
         topright = byte & 0b00001100
         bottomleft = byte & 0b00110000
