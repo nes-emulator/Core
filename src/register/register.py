6c3987083f410b54718cb8553194c39f5c33b3bd
Guilherme <guilhermeperrotta@live.com>
2019-11-10 22:03:32 -0300

    Add many new bit functions to Register

--- src/register/register.py
+++ src/register/register.py
@@ -24,6 +24,39 @@ class Register:
         mask = 1 << n
         self.value = self.value | mask
 
+    def clear_bit(self, n):
+        mask = 1 << n
+        self.value = self.value ^ mask
+
+    def change_bit(self, n, val):
+        if (val):
+            self.set_bit(n)
+        else:
+            self.clear_bit(n)
+
+    # Uses a list of bits to change values starting from low_bit
+    def change_bits(self, low_bit, bits):
+        current_bit = low_bit
+
+        for bit in bits:
+            if (bit):
+                self.set_bit(current_bit)
+            else:
+                self.clear_bit(current_bit)
+
+            current_bit += 1
+
+    # Returns a list of n bits starting with low_bit
+    def get_bits(self, low_bit, n):
+        current_bit = low_bit
+        bits = []
+
+        for i in range(0, n):
+            bits.append(self.get_bit(current_bit))
+            current_bit += 1
+
+        return bits
+
     def inc(self):
         self.value += 1
 
